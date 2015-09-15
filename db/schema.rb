0bdb1c58b37283f6a51bdc274b91c3f2891a1cc0
Evgen1983 <ebaranov1983@gmail.com>
2015-09-15 14:21:24 +0300

    logo

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema.define(version: 20150812130314) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
