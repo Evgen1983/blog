8ed9e6cee0a9ccd4d21ea53ac22bf49951c15379
Evgen1983 <ebaranov1983@gmail.com>
2015-09-23 17:10:14 +0300

    finish modeling users

--- db/migrate/20150812130314_create_posts.rb
+++ db/migrate/20150812130314_create_posts.rb
@@ -3,7 +3,8 @@ class CreatePosts < ActiveRecord::Migration
     create_table :posts do |t|
       t.string :title
       t.text :body
-
+      
+      
       t.timestamps null = false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150916090933) do
+ActiveRecord::Schema.define(version: 20150923135018) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "name"
@@ -28,6 +28,19 @@ ActiveRecord::Schema.define(version: 20150916090933) do
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
